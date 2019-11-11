493a3a6c3a31625d6d680c1070b2f26e819bd3d4
Sorin Chis <kish.sorin@yahoo.com>
2019-11-11 20:16:24 +0200

    Updates

---
+++ precache-manifest.1ec23d879b60f200afffe8b76e97acd4.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d677e9d1ffbbceda1833857c0facb3af",
+    "url": "/SorinC6/order-book/index.html"
+  },
+  {
+    "revision": "8963550791f63bc2d88c",
+    "url": "/SorinC6/order-book/static/css/main.0344b5d9.chunk.css"
+  },
+  {
+    "revision": "38f39dcb86273fbcf1e0",
+    "url": "/SorinC6/order-book/static/js/2.45080e90.chunk.js"
+  },
+  {
+    "revision": "8963550791f63bc2d88c",
+    "url": "/SorinC6/order-book/static/js/main.3453cc51.chunk.js"
+  },
+  {
+    "revision": "3cd6085113cb4f91390c",
+    "url": "/SorinC6/order-book/static/js/runtime-main.4672afc9.js"
+  }
+]);
---
+++ service-worker.js
@@ -0,0 +1,39 @@
+/**
+ * Welcome to your Workbox-powered service worker!
+ *
+ * You'll need to register this file in your web app and you should
+ * disable HTTP caching for this file too.
+ * See https://goo.gl/nhQhGp
+ *
+ * The rest of the code is auto-generated. Please don't update this file
+ * directly; instead, make changes to your Workbox build configuration
+ * and re-run your build process.
+ * See https://goo.gl/2aRDsh
+ */
+
+importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
+
+importScripts(
+  "/SorinC6/order-book/precache-manifest.1ec23d879b60f200afffe8b76e97acd4.js"
+);
+
+self.addEventListener('message', (event) => {
+  if (event.data && event.data.type === 'SKIP_WAITING') {
+    self.skipWaiting();
+  }
+});
+
+workbox.core.clientsClaim();
+
+/**
+ * The workboxSW.precacheAndRoute() method efficiently caches and responds to
+ * requests for URLs in the manifest.
+ * See https://goo.gl/S9QRab
+ */
+self.__precacheManifest = [].concat(self.__precacheManifest || []);
+workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
+
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/SorinC6/order-book/index.html"), {
+  
+  blacklist: [/^\/_/,/\/[^\/?]+\.[^\/]+$/],
+});
